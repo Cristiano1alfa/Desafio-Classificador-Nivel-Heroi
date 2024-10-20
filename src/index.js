2c4d8dacb5a4e122b97fb269f5a3253808f67da2
cristiano1alfa <cristiano1alfapro@gmail.com>
2024-10-20 01:43:14 -0300

    Projeto Desafio

---
+++ src/index.js
@@ -0,0 +1,63 @@
+let nome = "Ferro"
+let nome2 = "Bronze"
+let nome3 = "Prata"
+let nome4 = "Ouro"
+let nome5 = "Platina"
+let nome6 = "Ascendente"
+let nome7 = "Imortal"
+let nome8 = "Radiante"
+
+let Qtde_XP = "9005" //Digite o nº do nível do XP
+
+	if(Qtde_XP<=1000){ //let nome-Ferro
+    if(Qtde_XP<1001)
+        console.log("Heroi de nome: " +nome)
+        console.log("Está no nível: " +Qtde_XP)     
+   }
+   
+   if(Qtde_XP>=1001){ //let nome2-Bronze
+   if(Qtde_XP<=2000){
+        console.log("Heroi de nome: " +nome2)
+        console.log("Está no nível: " +Qtde_XP)   
+   }  
+   }
+   
+   if(Qtde_XP>=2001){ //let nome3-Prata
+   if(Qtde_XP<=5000){
+        console.log("Heroi de nome: " +nome3)
+        console.log("Está no nível: " +Qtde_XP)     
+   }
+   } 
+   
+   if(Qtde_XP>=5001){ //let nome4-Ouro
+   if(Qtde_XP<=7000){
+        console.log("Heroi de nome: " +nome4)
+        console.log("Está no nível: " +Qtde_XP)     
+   }
+   } 
+   
+   if(Qtde_XP>=7001){ //let nome5-Platina
+   if(Qtde_XP<=8000){
+        console.log("Heroi de nome: " +nome5)
+        console.log("Está no nível: " +Qtde_XP)     
+   }
+   } 
+   
+   if(Qtde_XP>=8001){ //let nome6-Ascendente
+   if(Qtde_XP<=9000){
+        console.log("Heroi de nome: " +nome6)
+        console.log("Está no nível: " +Qtde_XP)     
+   }
+   } 
+   
+   if(Qtde_XP>=9001){ //let nome7-Imortal
+   if(Qtde_XP<=10000){
+        console.log("Heroi de nome: " +nome7)
+        console.log("Está no nível: " +Qtde_XP)     
+   }
+   } 
+   
+   if(Qtde_XP>=10001){ //let nome8-Radiante 
+        console.log("Heroi de nome: " +nome8)
+        console.log("Está no nível: " +Qtde_XP)     
+   }
